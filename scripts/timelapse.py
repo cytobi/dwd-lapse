eb7bebed009921fcb60c33bf4f77f5d61195138e
cytobi <tobi81548@gmail.com>
2023-02-06 20:00:09 +0100

    better timelapse names

--- scripts/timelapse.py
+++ scripts/timelapse.py
@@ -1,11 +1,12 @@
 # call to make a timelapse from images in results folder
-# python3 timelapse.py [location] [name_addition] [fps]
+# python3 timelapse.py [location] [name_addition] [fps] [video_name]
 
 print("Loading libraries...")
 import sys
 import os
 from dotenv import load_dotenv
 import cv2
+import time
 
 print("Loading .env file...")
 load_dotenv()
@@ -24,7 +25,11 @@ fps = 18
 if len(sys.argv) > 3:
     fps = sys.argv[3]
 
-video_name = img_folder + '/video.mp4'
+video_name = location + '_' + name_addition + str(time.time()).replace(".", "-") + ".mp4"
+if len(sys.argv) > 4:
+    video_name = sys.argv[4] + ".mp4"
+
+video_name = img_folder + '/' + video_name
 
 print("Loading images...")
 images = [img for img in os.listdir(img_folder) if img.endswith(".jpg") and location in img and name_addition in img]
